a5a6a8471e41902045c7c926ea44a8b1db268cd9
Ari Maulana Firmansyah <65420719+arimaulanafirmansyah@users.noreply.github.com>
2021-09-14 10:14:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089', '6281334177340'] // Put your number here
+global.owner = ['6285157792618'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -20,7 +20,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
+global.packname = '2021 COPYRIGHT BY'
 global.author = 'AMFCODE'
 
 global.multiplier = 69 // The higher, The harder levelup
